4ab828e4e03ce6b5c6ddaaecf37d2ed587fe1bdb
hossammosh <37549426+hossammosh@users.noreply.github.com>
2025-08-06 13:14:17 +0300

    second trial 6 aug

--- lib/train/admin/settings.py
+++ lib/train/admin/settings.py
@@ -1,5 +1,5 @@
 from lib.train.admin.environment import env_settings
-
+from lib.config.seqtrack.phases import get_phases_config
 
 class Settings:
     """ Training settings, e.g. the paths to datasets and networks."""
@@ -13,4 +13,24 @@ class Settings:
         self.sample_per_epoch = 0
         self.top_sample_ratio = 0.0  # Default value
         self.top_selected_samples = 0  # Default value
+        self.phases = get_phases_config()
 
+
+
+# from lib.train.admin.environment import env_settings
+#
+#
+#
+# class Settings:
+#     """ Training settings, e.g. the paths to datasets and networks."""
+#     def __init__(self):
+#         self.set_default()
+#
+#     def set_default(self):
+#         self.env = env_settings()
+#         self.use_gpu = True
+#         self.selected_sampling = False
+#         self.sample_per_epoch = 0
+#         self.top_sample_ratio = 0.0  # Default value
+#         self.top_selected_samples = 0  # Default value
+#
